f09018ea26a79e2acf86020da93ed69c1528a548
canghaiyisu666 <340541293@qq.com>
2024-11-18 15:09:33 +0800

    新增数据集处理脚本： 1.设置训练集，验证集，测试集的比例并且生成对应的文件列表 2.针对每一种数据集，在路径后加入标签

---
+++ 26-马绍龙-西安/week14/voc2faster-rcnn.py
@@ -0,0 +1,54 @@
+import os
+import random
+
+# 定义XML文件路径和保存基础路径
+xmlfilepath = r'./VOCdevkit/VOC2007/Annotations'
+saveBasePath = r"./VOCdevkit/VOC2007/imageSets/Main/"
+
+# 定义训练验证集和训练集的比例
+trainval_percent = 0.9
+train_percent = 0.8
+
+# 获取XML文件夹中的所有文件，并筛选出.xml文件
+temp_xml = os.listdir(xmlfilepath)
+total_xml = []
+for xml in temp_xml:
+    if xml.endswith(".xml"):
+        total_xml.append(xml)
+
+# 计算总文件数，并生成索引列表
+num = len(total_xml)
+list = range(num)
+# 根据比例计算训练验证集和训练集的大小
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+# 随机选择训练验证集和训练集的索引
+trainval = random.sample(list, tv)
+train = random.sample(trainval, tr)
+
+# 打印训练验证集和训练集的大小
+print("train and val size", tv)
+print("train size", tr)
+# 打开文件以写入相应的索引
+ftrainval = open(os.path.join(saveBasePath, 'trainval.txt'), 'w')
+ftest = open(os.path.join(saveBasePath, 'test.txt'), 'w')
+ftrain = open(os.path.join(saveBasePath, 'train.txt'), 'w')
+fval = open(os.path.join(saveBasePath, 'val.txt'), 'w')
+
+# 遍历索引列表，根据索引写入相应的文件名到对应的文件中
+for i in list:
+    name = total_xml[i][:-4] + '\n'
+    if i in trainval:
+        ftrainval.write(name)
+        if i in train:
+            ftrain.write(name)
+        else:
+            fval.write(name)
+    else:
+        ftest.write(name)
+
+# 关闭所有打开的文件
+ftrainval.close()
+ftrain.close()
+fval.close()
+ftest.close()
---
+++ 26-马绍龙-西安/week14/voc_annotation.py
@@ -0,0 +1,69 @@
+import xml.etree.ElementTree as ET
+from os import getcwd
+
+sets = [('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+
+wd = getcwd()
+classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog",
+           "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+
+
+def convert_annotation(year, image_id, list_file):
+    """
+    将图像的标注信息转换为所需格式并写入到 list_file 中。
+
+    该函数从 Pascal VOC 数据集中的 XML 文件读取图像的标注信息，提取图像中每个对象的信息，
+    并以指定格式将这些信息写入到 list_file 中。写入的信息包括图像路径、对象的边界框坐标以及对象类别。
+
+    参数:
+    - year (str): 数据集年份，例如 '2007' 或 '2012'。
+    - image_id (str): 图像的唯一标识符。
+    - list_file (file object): 用于写入转换后标注信息的文件对象。
+
+    返回:
+    无
+    """
+    # 打开 XML 标注文件
+    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml' % (year, image_id))
+
+    # 解析 XML 文件
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+
+    # 检查是否存在对象标签
+    if root.find('object') is None:
+        return
+
+    # 写入图像路径
+    list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg' % (wd, year, image_id))
+
+    # 对有对象的图片记录标签转换为classes的索引后也写入文件中
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+
+        # 跳过不在类别列表中的对象或困难对象
+        if cls not in classes or int(difficult) == 1:
+            continue
+
+        # 获取类别的索引
+        cls_id = classes.index(cls)
+
+        # 获取边界框信息
+        xmlbox = obj.find('bndbox')
+        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
+
+        # 写入边界框坐标和类别索引
+        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+
+    # 写入换行符
+    list_file.write('\n')
+
+
+
+for year, image_set in sets:
+    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt' % (year, image_set)).read().strip().split()
+    list_file = open('%s_%s.txt' % (year, image_set), 'w')
+    for image_id in image_ids:
+        convert_annotation(year, image_id, list_file)
+    list_file.close()
